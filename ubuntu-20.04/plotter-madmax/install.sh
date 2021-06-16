6a060a4a16bea52a4ed40509d097f6fcaf8b264a
smerch <stas@belochka.kz>
2021-06-17 01:09:02 +0600

    - change plotter.madmax create folders ssdTMP & plots

--- ubuntu-20.04/plotter-madmax/install.sh
+++ ubuntu-20.04/plotter-madmax/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir "$(pwd)/../../plots"
+mkdir "$(pwd)/../../ssdTMP"
+
 sudo apt install -y libsodium-dev cmake g++ git
 # Checkout the source and install
 git clone https://github.com/madMAx43v3r/chia-plotter.git
