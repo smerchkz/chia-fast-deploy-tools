ce9108d14cb3c46b744b007c7ccfecb470043bde
smerch <stas@belochka.kz>
2021-06-17 03:44:04 +0600

    - add to storage.google gsutils support

--- ubuntu-20.04/storage-google/install.sh
+++ ubuntu-20.04/storage-google/install.sh
@@ -17,3 +17,15 @@ export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gstorage-key.json"
 #mount
 mkdir "$(pwd)/../../storage"
 gcsfuse $basketName $(pwd)/../../storage
+
+
+#[gsutils]
+#https://cloud.google.com/storage/docs/gsutil_install#deb
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+
+gcloud auth activate-service-account --key-file=$(pwd)/gstorage-key.json
